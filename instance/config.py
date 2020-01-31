4644cf5951993f6d1172dc2b98debba610535487
Don Moses <githinjidon@gmail.com>
2020-01-31 12:54:03 +0300

    deployment to heroku

---
+++ instance/config.py
@@ -0,0 +1,2 @@
+MOVIE_API_KEY = "421ef045850f4d3b522c45d170ea9a33"
+SECRET_KEY = "watchlistd0n"
---
+++ start.sh
@@ -0,0 +1,4 @@
+ export MOVIE_API_KEY="421ef045850f4d3b522c45d170ea9a33"
+export SECRET_KEY="watchlistd0n"
+
+python3.6 manage.py server
